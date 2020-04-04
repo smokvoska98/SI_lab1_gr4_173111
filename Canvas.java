e39e122b401945869631e1fa7fae83c3b8888cc1
smokvoska98 <smokvoska.98@gmail.com>
2020-04-04 19:29:21 +0200

    Canvas.java file added

---
+++ Canvas.java
@@ -0,0 +1,33 @@
+class Canvas
+{
+    List<Point> points;
+    
+    void Canvas()
+    {
+        points = new List<Point>();
+    }
+    
+    void addPoints(int x, int y)
+    {
+        Point point = new Point(x, y);
+        points.add(point);
+    }
+    
+    void moveAllUpward()
+    {
+        for(int i=0; i<points.size(); i++)
+            points.get(i).move('S', 'U');
+    }
+    
+    float getAvgX()
+    {
+        float avg = 0;
+        for(int i=0; i<points.size(); i++)
+            avg += points.get(i);
+            
+        return avg / points.size(); 
+    }
+}
+
+
+
