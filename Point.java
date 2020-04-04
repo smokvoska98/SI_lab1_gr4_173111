01e48358479e53be11e49a4c7c343f91207a8723
smokvoska98 <smokvoska.98@gmail.com>
2020-04-04 19:05:56 +0200

    point class finished

--- Point.java
+++ Point.java
@@ -4,6 +4,45 @@ class Point {
 
         String color;
    	//TODO add new variable
+         public String getId() {
+			return id;
+		}
+
+		public void setId(String id) {
+			this.id = id;
+		}
+
+		public double getX() {
+			return x;
+		}
+
+		public void setX(double x) {
+			this.x = x;
+		}
+
+		public double getY() {
+			return y;
+		}
+
+		public void setY(double y) {
+			this.y = y;
+		}
+
+		public String getColor() {
+			return color;
+		}
+
+		public void setColor(String color) {
+			this.color = color;
+		}
+
+		public Point(String id, double x, double y, String color) {
+			super();
+			this.id = id;
+			this.x = x;
+			this.y = y;
+			this.color = color;
+		}
 
 	//TODO constructor
 
@@ -11,10 +50,21 @@ class Point {
 
 	public void move (char xDirection, char yDirection) {
 		//TODO
+             if(xDirection.equals('L'))
+            x--;
+        else if(xDirection.equals('R'))
+            x++;
+            
+        if(yDirection.equals('U'))
+            y++;
+        else if(yDirection.equals('D'))
+            y--;
 	}
 
 	public void draw () {
-		//TODO
+                 System.out.print("("+x+", "+y+")"); 
+		
+//TODO
 	}
 
 
